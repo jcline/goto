81312fc5a93e908fc6eaff60dbb601890b8beee5
James Cline <jamesclinedev+github@gmail.com>
2014-12-29 02:16:29 -0800

    help: preliminary support for refusing commands
    This needs to be moved into the configuration stuff, but it's a start.
    The idea here is to allow the bot to respond to commands it doesn't
    support (my use case is to insult people looking for anime XDCC bots in
    our channel).

--- plugins/help.go
+++ plugins/help.go
@@ -8,7 +8,7 @@ import (
 
 type Help struct {
 	plugin
-	help map[string]string
+	help, no map[string]string
 }
 
 func (plug *Help) Setup(write chan IRCMessage, conf PluginConf) {
@@ -23,6 +23,7 @@ func (plug *Help) Setup(write chan IRCMessage, conf PluginConf) {
 		regexPrefix + `(?:please[,] ){0,1}tell me (?P<command>about) yourself(?:[,] please){0,1}` + regexPostfix,
 		regexPrefix + `(?:please |would you please ){0,1}(?P<command>help) me` + regexPostfix,
 		regexPrefix + `how do I search for (?P<command>anime|manga)` + regexPostfix,
+		`^[.!](?P<shitlord>blist|akick|list)$`,
 		//regexPrefix + `` + regexPostfix,
 	}
 
@@ -34,6 +35,12 @@ func (plug *Help) Setup(write chan IRCMessage, conf PluginConf) {
 		"anime": "!anime Galaxy Express 999",
 		"manga": "!manga Galaxy Express 999",
 	}
+
+	plug.no = map[string]string{
+		"blist": "How dare you! I'm no criminal!",
+		"list": "私は断る",
+		"akick": "I'm not your slave!",
+	}
 	go plug.Action()
 	return
 }
@@ -41,6 +48,7 @@ func (plug *Help) Setup(write chan IRCMessage, conf PluginConf) {
 func (plug Help) Action() {
 	for msg := range plug.event {
 		key := ""
+		refusal := false
 		query, err := getMatch(plug.match, &msg.Msg)
 		if err == nil {
 			for index, val := range query[0] {
@@ -48,15 +56,26 @@ func (plug Help) Action() {
 					continue
 				}
 
-				if val != "" && plug.match.SubexpNames()[index] == "command" {
-					key = val
+				category := plug.match.SubexpNames()[index]
+				if val != "" {
+					switch category {
+					case "command":
+						key = val
+					case "shitlord":
+						refusal = true
+						key = val
+					}
 				}
 			}
 
-			if val, ok := plug.help[key]; ok {
-				plug.write <- IRCMessage{Channel: msg.User, Msg: val, User: msg.User, When: msg.When}
-			} else {
-				plug.write <- IRCMessage{Channel: msg.User, Msg: "┐('～`；)┌", User: msg.User, When: msg.When}
+			if !refusal {
+				if val, ok := plug.help[key]; ok {
+					plug.write <- IRCMessage{Channel: msg.User, Msg: val, User: msg.User, When: msg.When}
+				} else {
+					plug.write <- IRCMessage{Channel: msg.User, Msg: "┐('～`；)┌", User: msg.User, When: msg.When}
+				}
+			} else if val, ok := plug.no[key]; ok {
+					plug.write <- IRCMessage{Channel: msg.User, Msg: val, User: msg.User, When: msg.When}
 			}
 		} else {
 			log.Println(err)
